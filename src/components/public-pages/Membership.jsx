1cd1f217d9935383c0c47d510cf266cda27abce9
nafi-islam <nafi.islam321@gmail.com>
2025-11-11 09:29:18 -0600

    update import because of case sensitivity error

--- src/components/public-pages/Membership.jsx
+++ src/components/public-pages/Membership.jsx
@@ -5,7 +5,7 @@ import "../../styles/membership.css";
 import Ben_Snavely from "../../assets/img/2023Headshots/Ben_Snavely.JPG";
 import Jacob_Technik from "../../assets/img/2023Headshots/Jacob_Technik.JPG";
 import Joshua_Mayhugh from "../../assets/img/2023Headshots/Joshua_Mayhugh.JPG";
-import Manuel_Pandavila from "../../assets/img/2023Headshots/Manuel_Pandavila.JPG";
+import Manuel_Pandavila from "../../assets/img/2023Headshots/Manuel_PanDavila.JPG";
 import Jacob_Hargreaves from "../../assets/img/2023Headshots/Jacob_Hargreaves.JPG";
 
 // non-executive staff
